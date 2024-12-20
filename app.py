bb2b5babd0bd8b19bf910f01abb8d7b707801d31
Cr0me1ve <kvilkov01@gmail.com>
2024-12-20 20:08:46 +0300

    v2

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'lab1_front\index.html')
+    return send_file('../lab1_front/index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.json.get('text', '')
     if data:
-        with open('data.txt', 'a') as f:
+        with open('lab1_back\data.txt', 'a') as f:
             f.write(data + '\n')
         return jsonify({}), 200
     return jsonify({}), 400
+
+@app.route('/get_info', methods=['GET'])
+def get_info():
+    with open('lab1_back\data.txt', 'r') as f:
+        data = f.read()
+    return jsonify({'data': data}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
